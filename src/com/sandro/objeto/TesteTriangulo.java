e4abbc533d20c67d0bca1ac447f4d7ffdebf18cb
Sandro Aparecido <sandro030486@gmail.com>
2020-12-09 16:55:46 -0300

    Criando do Objeto Triangulo

---
+++ src/com/sandro/objeto/TesteTriangulo.java
@@ -0,0 +1,41 @@
+package com.sandro.objeto;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class TesteTriangulo {
+
+    public static void main(String[] args) {
+
+            Locale.setDefault(Locale.US);
+            Scanner scan = new Scanner(System.in);
+
+            Triangulo x, y;
+            x = new Triangulo();
+            y = new Triangulo();
+
+            System.out.println("Insira as medidas do Triangulo X: ");
+            x.a = scan.nextDouble();
+            x.b = scan.nextDouble();
+            x.c= scan.nextDouble();
+            System.out.println("Insira as medidas do Triangulo X: ");
+            y.a = scan.nextDouble();
+            y.b =  scan.nextDouble();
+            y.c = scan.nextDouble();
+
+            double p = (x.a + x.b + x.c) / 2.0;
+            double areaX = Math.sqrt(p * (p - x.a) * (p - x.b) * (p - x.c));
+
+            p = (y.a + y.b + y.c) / 2.0;
+            double areaY = Math.sqrt(p * (p - y.a) * (p - y.b) * (p - y.c));
+            System.out.printf("Triangle X area: %.4f%n", areaX);
+            System.out.printf("Triangle Y area: %.4f%n", areaY);
+            if (areaX > areaY) {
+                System.out.println("Larger area: X");
+            }
+            else {
+                System.out.println("Larger area: Y");
+            }
+            scan.close();
+    }
+}
---
+++ src/com/sandro/objeto/Triangulo.java
@@ -0,0 +1,9 @@
+package com.sandro.objeto;
+
+public class Triangulo {
+    public double a;
+    public double b;
+    public double c;
+
+
+}
